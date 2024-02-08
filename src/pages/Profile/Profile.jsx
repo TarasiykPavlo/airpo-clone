9cb2e42df9db0736600bb6f67b9df5d94d3d42f0
KripakDebug <kastrula123@gmail.com>
2024-02-08 15:35:21 +0200

    add flip card

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Profile.scss";
-import { Avatar, ConfigProvider, message, theme } from "antd";
+import { Avatar, ConfigProvider, theme } from "antd";
 import iconPencil from "./../../assets/pencil.svg";
 import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
 import ButtonForIcon from "../../ui/ButtonForIcon";
@@ -11,7 +11,6 @@ import ProfileCard from "./ProfileCard";
 import { getClientData } from "../../services/apiAuthClient";
 
 function Profile() {
-  const [messageShow, messageContext] = message.useMessage();
   const { user } = useUser();
   const { picture, name } = user.user_metadata;
   const { data: client } = useAuthClient(user.id);
@@ -21,7 +20,6 @@ function Profile() {
         algorithm: theme.darkAlgorithm,
       }}
     >
-      {messageContext}
       <div className="wrapper">
         <div className="profile">
           <div className="profile__head">
@@ -61,9 +59,6 @@ function Profile() {
 
   function changeButtonActive(id) {
     setActiveButton(id);
-  }
-  function infoMessage(message) {
-    messageShow.info(message);
   }
 }
 
