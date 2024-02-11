f13c2fa0685b84aa6b950eb7bd9fbcd10034b1c1
kastastin <kostya242421@gmail.com>
2024-02-11 20:27:42 +0200

    refactoring

--- src/pages/Profile/ProfileCard.jsx
+++ src/pages/Profile/ProfileCard.jsx
@@ -1,203 +1,196 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, message } from "antd";
-import ProfilePaymentsHistoryItem from "./ProfilePaymentsHistoryItem";
-import ButtonForIcon from "../../ui/ButtonForIcon";
 import { InfoCircleOutlined, CopyOutlined } from "@ant-design/icons";
+
+import ButtonForIcon from "../../ui/ButtonForIcon";
+import ProfilePaymentsHistoryItem from "./ProfilePaymentsHistoryItem";
 import ProfileReferralHistoryItem from "./ProfileReferralHistoryItem";
+import { getDateAicoinHistory } from "../../utils/helpers";
 
 function ProfileCard({ userId, aicoin, name, email, clientAicoinHistory }) {
-  const [activeButton, setActiveButton] = useState("personalBtn");
-  const [messageShow, messageContext] = message.useMessage();
+	const [activeButton, setActiveButton] = useState("personalBtn");
+	const [messageShow, messageContext] = message.useMessage();
 
-  var values = clientAicoinHistory?.aicoin || [0];
-  var dates = clientAicoinHistory?.created_at || [];
+	const [matches, setMatches] = useState(
+		window.matchMedia("(max-width: 1140px)").matches
+	);
 
-  function getDateAicoinHistory(date){
-    const d = new Date(date);
-    return d.getDate() + 'd-' + (d.getMonth()+1) + 'm-' + d.getFullYear() +"y"
-  }
-  const [matches, setMatches] = useState(
-    window.matchMedia("(max-width: 1140px)").matches
-  );
-  useEffect(() => {
-    window
-      .matchMedia("(max-width: 1140px)")
-      .addEventListener("change", (e) => setMatches(e.matches));
-  }, []);
+	const values = clientAicoinHistory?.aicoin || [0];
+	const dates = clientAicoinHistory?.created_at || [];
 
-  return (
-    <div className="cards-wrap">
-      {messageContext}
-      <div
-        className={activeButton === "personalBtn" ? "card active" : "card"}
-        id="personal"
-      >
-        <div
-          className="card__head-name"
-          id="personalBtn"
-          onClick={(e) => setActiveButton(e.target.id)}
-        >
-          Personal Area
-        </div>
-        <div className="card__info">
-          <div className="card__main-info">
-            <div className="card__user-info">
-              <div className="title">Рersonal information</div>
-              <div id="name" className="card__text">
-                Name: {name || "Name"}
-              </div>
-              <div id="email" className="card__text">
-                Email: {email || "aipro@gmail.com"}
-              </div>
-              <ButtonForIcon
-                icon={
-                  <InfoCircleOutlined
-                    style={{
-                      color: "#24A1E0",
-                      position: "absolute",
-                      top: "15px",
-                      right: "15px",
-                    }}
-                  />
-                }
-              />
-            </div>
-            <div className="ref-link">
-              Referral link:
-              <div className="link">{userId}</div>
-              <div className="ref-link-item">
-                <CopyOutlined
-                  style={{
-                    fontSize: "15px",
-                  }}
-                  onClick={() => {
-                    navigator.clipboard.writeText(
-                      userId || "00000000-0000-0000-0000-000000000000"
-                    );
-                    infoMessage("Copy link!");
-                  }}
-                />
-                <ButtonForIcon
-                  icon={
-                    <InfoCircleOutlined
-                      style={{ color: "#24A1E0", fontSize: "15px" }}
-                    />
-                  }
-                />
-              </div>
-            </div>
-            <Button type="primary" className="btn-check" block>
-              My applications
-            </Button>
-          </div>
-          <div className="card__pay">
-            <div className="title">
-              Payments
-              <div className="card__balance">Balance: {aicoin} AIC</div>
-            </div>
+	useEffect(() => {
+		window
+			.matchMedia("(max-width: 1140px)")
+			.addEventListener("change", (e) => setMatches(e.matches));
+	}, []);
 
-            <ul className="card__history">
-              {values.map((value, index) => (
-                <ProfilePaymentsHistoryItem
-                  key={Math.random()}
-                  value={value}
-                  date={dates[index]? getDateAicoinHistory(dates[index]) : "..."}
-                />
-              ))}
-              <Button type="primary" block>
-                Pay out
-              </Button>
-            </ul>
-          </div>
-          {matches && (
-            <Button
-              type="primary"
-              className="btn-check"
-              style={{ display: "block" }}
-              block
-            >
-              My applications
-            </Button>
-          )}
-        </div>
-      </div>
+	return (
+		<div className="cards-wrap">
+			{messageContext}
+			<div
+				className={activeButton === "personalBtn" ? "card active" : "card"}
+				id="personal"
+			>
+				<div
+					className="card__head-name"
+					id="personalBtn"
+					onClick={(e) => setActiveButton(e.target.id)}
+				>
+					Personal Area
+				</div>
+				<div className="card__info">
+					<div className="card__main-info">
+						<div className="card__user-info">
+							<div className="title">Рersonal information</div>
+							<div id="name" className="card__text">
+								Name: {name}
+							</div>
+							<div id="email" className="card__text">
+								Email: {email}
+							</div>
+							<ButtonForIcon
+								icon={
+									<InfoCircleOutlined
+										style={{
+											color: "#24A1E0",
+											position: "absolute",
+											top: "15px",
+											right: "15px",
+										}}
+									/>
+								}
+							/>
+						</div>
+						<div className="ref-link">
+							Referral link:
+							<div className="link">{userId}</div>
+							<div className="ref-link-item">
+								<CopyOutlined
+									style={{
+										fontSize: "15px",
+									}}
+									onClick={() => {
+										navigator.clipboard.writeText(userId);
+										messageShow.info("Copy link!");
+									}}
+								/>
+								<ButtonForIcon
+									icon={
+										<InfoCircleOutlined
+											style={{ color: "#24A1E0", fontSize: "15px" }}
+										/>
+									}
+								/>
+							</div>
+						</div>
+						<Button type="primary" className="btn-check" block>
+							My applications
+						</Button>
+					</div>
+					<div className="card__pay">
+						<div className="title">
+							Payments
+							<div className="card__balance">Balance: {aicoin} AIC</div>
+						</div>
 
-      <div
-        className={activeButton === "referralBtn" ? "card active" : "card"}
-        id="referral"
-      >
-        <div
-          className="card__head-name"
-          id="referralBtn"
-          onClick={(e) => setActiveButton(e.target.id)}
-        >
-          Referral system
-        </div>
-        <div className="card__info">
-          <div className="card__main-info">
-            <div className="card__total-earnings">
-              Total earnings: <span>{aicoin} AiCoin</span>
-              <ButtonForIcon
-                icon={
-                  <InfoCircleOutlined
-                    style={{ color: "#24A1E0", fontSize: "15px" }}
-                  />
-                }
-              />
-            </div>
-            <div className="ref-link">
-              Referral link:
-              <div className="link">
-                {userId || "00000000-0000-0000-0000-000000000000"}
-              </div>
-              <div className="ref-link-item">
-                <CopyOutlined
-                  style={{
-                    fontSize: "15px",
-                  }}
-                  onClick={() => {
-                    navigator.clipboard.writeText(
-                      userId || "00000000-0000-0000-0000-000000000000"
-                    );
-                    infoMessage("Copy link!");
-                  }}
-                />
-                <ButtonForIcon
-                  icon={
-                    <InfoCircleOutlined
-                      style={{ color: "#24A1E0", fontSize: "15px" }}
-                    />
-                  }
-                />
-              </div>
-            </div>
-          </div>
-          <div className="card__table-wrap">
-            <table className="card__table">
-              <thead>
-                <tr>
-                  <th scope="col">Referral ID</th>
-                  <th scope="col">Amount of earnings</th>
-                </tr>
-              </thead>
-              <tbody>
-                {values.map((value, index) => (
-                  <ProfileReferralHistoryItem
-                    key={Math.random()}
-                    value={value}
-                    RefId={userId}
-                  />
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-  function infoMessage(message) {
-    messageShow.info(message);
-  }
+						<ul className="card__history">
+							{clientAicoinHistory?.aicoin ||
+								[0].map((value, index) => (
+									<ProfilePaymentsHistoryItem
+										key={Math.random()}
+										value={value}
+										date={
+											dates[index] ? getDateAicoinHistory(dates[index]) : "..."
+										}
+									/>
+								))}
+							<Button type="primary" block>
+								Pay out
+							</Button>
+						</ul>
+					</div>
+					{matches && (
+						<Button
+							type="primary"
+							className="btn-check"
+							style={{ display: "block" }}
+							block
+						>
+							My applications
+						</Button>
+					)}
+				</div>
+			</div>
+
+			<div
+				className={activeButton === "referralBtn" ? "card active" : "card"}
+				id="referral"
+			>
+				<div
+					className="card__head-name"
+					id="referralBtn"
+					onClick={(e) => setActiveButton(e.target.id)}
+				>
+					Referral system
+				</div>
+				<div className="card__info">
+					<div className="card__main-info">
+						<div className="card__total-earnings">
+							Total earnings: <span>{aicoin} AiCoin</span>
+							<ButtonForIcon
+								icon={
+									<InfoCircleOutlined
+										style={{ color: "#24A1E0", fontSize: "15px" }}
+									/>
+								}
+							/>
+						</div>
+						<div className="ref-link">
+							Referral link:
+							<div className="link">{userId}</div>
+							<div className="ref-link-item">
+								<CopyOutlined
+									style={{
+										fontSize: "15px",
+									}}
+									onClick={() => {
+										navigator.clipboard.writeText(userId);
+										messageShow.info("Copy link!");
+									}}
+								/>
+								<ButtonForIcon
+									icon={
+										<InfoCircleOutlined
+											style={{ color: "#24A1E0", fontSize: "15px" }}
+										/>
+									}
+								/>
+							</div>
+						</div>
+					</div>
+					<div className="card__table-wrap">
+						<table className="card__table">
+							<thead>
+								<tr>
+									<th scope="col">Referral ID</th>
+									<th scope="col">Amount of earnings</th>
+								</tr>
+							</thead>
+							<tbody>
+								{values.map((value) => (
+									<ProfileReferralHistoryItem
+										key={Math.random()}
+										value={value}
+										RefId={userId}
+									/>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ProfileCard;
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,52 +1,72 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
 
-  if (error) throw new Error(error.message);
+	if (error) throw new Error(error.message);
 
-  return data;
+	return data;
 }
 
 export async function authGoogle() {
-  supabase.auth.signInWithOAuth({
-    provider: "google",
-  });
+	supabase.auth.signInWithOAuth({
+		provider: "google",
+	});
 }
 
 export async function signup({ fullName, email, password }) {
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      data: {
-        fullName,
-        avatar: "",
-      },
-    },
-  });
+	const { data, error } = await supabase.auth.signUp({
+		email,
+		password,
+		options: {
+			data: {
+				fullName,
+				avatar: "",
+			},
+		},
+	});
 
-  if (error) throw new Error(error.message);
+	if (error) throw new Error(error.message);
 
-  return data;
+	return data;
 }
 
 export async function getCurrentUser() {
-  const { data: session } = await supabase.auth.getSession();
+	const { data: session } = await supabase.auth.getSession();
 
-  if (!session.session) return null;
+	if (!session.session) return null;
 
-  const { data, error } = await supabase.auth.getUser();
+	const { data, error } = await supabase.auth.getUser();
 
-  if (error) throw new Error(error.message);
+	if (error) throw new Error(error.message);
 
-  return data?.user;
+	return data?.user;
 }
 
 export async function logout() {
-  const { error } = await supabase.auth.signOut();
-  if (error) throw new Error(error.message);
+	const { error } = await supabase.auth.signOut();
+	if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser(props) {
+	const fileName = `avatar-${Math.random()}`;
+
+	const { error: storageError } = await supabase.storage
+		.from("avatar")
+		.upload(fileName, props);
+
+	if (storageError) throw new Error(storageError.message);
+
+	const { data: updatedUser, error: userError } =
+		await supabase.auth.updateUser({
+			data: {
+				avatar: `${supabaseUrl}/storage/v1/object/public/avatar/${fileName}`,
+			},
+		});
+
+	if (userError) throw new Error(userError.message);
+	return updatedUser;
 }
