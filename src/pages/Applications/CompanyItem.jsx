5824fc3abcf844b5eeda048fcf1e9fd3fe82e634
beanomen4 <beanomen4@gmail.com>
2024-03-06 18:24:48 +0200

    add config, fix bugs and code optimization

--- src/pages/Applications/CompanyItem.jsx
+++ src/pages/Applications/CompanyItem.jsx
@@ -7,6 +7,7 @@ import {
 } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { postResponseToLink } from "../../services/apiAplication";
+import { linksResponse } from "../../utils/constants";
 
 function CompanyItem({
   companyName,
@@ -19,51 +20,46 @@ function CompanyItem({
 }) {
   const navigate = useNavigate();
 
-  async function handlePlay() {
-    if (!active || (botId === null && apiId === null) || selectTemplateId === null) return;
-  
-    const link = "http://46.175.151.65:8000/api/start_telegram_sending";
-    const codeData = {
-      companyId: companyId,
-      isRunning: isRunning
-    };
-  
-    await postResponseToLink(codeData, link);
-    navigate("/applications")
-  }
+  const companyStatus =
+    !active || (botId === null && apiId === null) || selectTemplateId === null;
 
-  async function handleStop() {
-    if (!active || (botId === null && apiId === null) || selectTemplateId === null) return;
-  
-    const link = "http://46.175.151.65:8000/api/stop_telegram_sending";
+  async function handlePlayAndStop(link) {
+    if (companyStatus) return;
+
     const codeData = {
       companyId: companyId,
-      isRunning: isRunning
+      isRunning: isRunning,
     };
-  
+
     await postResponseToLink(codeData, link);
-    navigate("/applications")
+    navigate("/applications");
   }
 
   return (
     <li className="application__item">
       <div className="application__item-left">
-        {!active || (botId === null && apiId === null) || selectTemplateId === null ? (
+        {companyStatus && (
           <CloseCircleFilled className="application__stop-icon" />
-        ) : (
-          ""
         )}
         <span className="application__name">{companyName}</span>
       </div>
 
       <div className="application__item-right">
         <ExclamationCircleFilled className="application__error-icon" />
-        {!active || botId === null && apiId === null || selectTemplateId === null ? (
+        {companyStatus ? (
           <CloseCircleFilled className="application__stop-icon" />
         ) : isRunning ? (
-          <PauseCircleFilled className="application__pause-icon" onClick={handleStop} />
+          <PauseCircleFilled
+            className="application__pause-icon"
+            onClick={() => handlePlayAndStop(linksResponse.stopTelegramSending)}
+          />
         ) : (
-          <PlayCircleFilled className="application__launch-icon" onClick={handlePlay} />
+          <PlayCircleFilled
+            className="application__launch-icon"
+            onClick={() =>
+              handlePlayAndStop(linksResponse.startTelegramSending)
+            }
+          />
         )}
 
         <MoreOutlined
