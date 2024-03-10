13dfdd131613bd93726b30635681ff0e7ea50da9
kastastin <kostya242421@gmail.com>
2024-03-10 20:12:31 +0200

    terms of the user agreement

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,113 +1,95 @@
-import { Button, Checkbox, Form, Input, Spin, message } from "antd";
+import { Button, Form, Input, Spin, message } from "antd";
 
 import { useLogin } from "./useLogin";
 import "../../pages/Auth/Auth.scss";
 
 function LoginForm({ children }) {
-  const { login, isLoading } = useLogin();
+	const { login, isLoading } = useLogin();
 
-  const [form] = Form.useForm();
-  const [messageApi, contextHolder] = message.useMessage();
+	const [form] = Form.useForm();
+	const [messageApi, contextHolder] = message.useMessage();
 
-  const onFinish = ({ email, password }) => {
-    login(
-      { email, password },
-      {
-        onSettled: () => form.resetFields(),
-        onError: () => {
-          messageApi.open({
-            type: "error",
-            content: "Provided email or password are incorrect",
-          });
-        },
-      }
-    );
-  };
+	const onFinish = ({ email, password }) => {
+		login(
+			{ email, password },
+			{
+				onSettled: () => form.resetFields(),
+				onError: () => {
+					messageApi.open({
+						type: "error",
+						content: "Provided email or password are incorrect",
+					});
+				},
+			}
+		);
+	};
 
-  return (
-    <>
-      {contextHolder}
+	return (
+		<>
+			{contextHolder}
 
-      <Form
-        form={form}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        autoComplete="off"
-      >
-        {children}
+			<Form form={form} onFinish={onFinish} autoComplete="off">
+				{children}
 
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Please input your email!",
-            },
-          ]}
-          style={{ width: "100%", margin: 0 }}
-        >
-          <Input
-            type="email"
-            placeholder="Email"
-            className="auth__input"
-            disabled={isLoading}
-          />
-        </Form.Item>
+				<Form.Item
+					name="email"
+					rules={[
+						{
+							required: true,
+							message: "Please input your email!",
+						},
+					]}
+					style={{ width: "100%", margin: 0 }}
+				>
+					<Input
+						type="email"
+						placeholder="Email"
+						className="auth__input"
+						disabled={isLoading}
+					/>
+				</Form.Item>
 
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-          ]}
-          style={{ width: "100%", margin: 0 }}
-        >
-          <Input.Password
-            type="password"
-            placeholder="Password"
-            className="auth__input"
-            disabled={isLoading}
-          />
-        </Form.Item>
+				<Form.Item
+					name="password"
+					rules={[
+						{
+							required: true,
+							message: "Please input your password!",
+						},
+					]}
+					style={{ width: "100%", margin: 0 }}
+				>
+					<Input.Password
+						type="password"
+						placeholder="Password"
+						className="auth__input"
+						disabled={isLoading}
+					/>
+				</Form.Item>
 
-        <Form.Item
-          name="remember"
-          valuePropName="checked"
-          style={{
-            width: "100%",
-            display: "grid",
-            placeItems: "center",
-            marginBottom: "5px",
-          }}
-        >
-          <Checkbox disabled={isLoading}>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item
-          style={{
-            width: "100%",
-            display: "grid",
-            placeItems: "center",
-            marginBottom: 0,
-          }}
-        >
-          <Button
-            ghost={isLoading}
-            type="primary"
-            shape="round"
-            size="large"
-            className="auth__button"
-            disabled={isLoading}
-            htmlType="submit"
-          >
-            {isLoading ? <Spin /> : "Log In"}
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
-  );
+				<Form.Item
+					style={{
+						width: "100%",
+						display: "grid",
+						placeItems: "center",
+						marginBottom: 0,
+					}}
+				>
+					<Button
+						ghost={isLoading}
+						type="primary"
+						shape="round"
+						size="large"
+						className="auth__button"
+						disabled={isLoading}
+						htmlType="submit"
+					>
+						{isLoading ? <Spin /> : "Log In"}
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
+	);
 }
 
 export default LoginForm;
--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,131 +1,230 @@
-import { Button, Form, Input, Spin, message } from "antd";
+import { useState } from "react";
+import { Button, Checkbox, Form, Input, Spin, Modal, message } from "antd";
 
 import { useSignup } from "./useSignup";
 import "../../pages/Auth/Auth.scss";
 
 function SignupForm({ children }) {
-  const { signup, isLoading } = useSignup();
+	const { signup, isLoading } = useSignup();
 
-  const [form] = Form.useForm();
-  const [messageApi, contextHolder] = message.useMessage();
+	const [form] = Form.useForm();
+	const [messageApi, contextHolder] = message.useMessage();
+	const [isTermsAccepted, setIsTermsAccepted] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const onFinish = ({ fullName, email, password }) => {
-    signup(
-      { fullName, email, password },
-      {
-        onSettled: () => form.resetFields(),
-        onSuccess: () => {
-          messageApi.open({
-            type: "success",
-            duration: 5,
-            content:
-              "Account successfully created! Verify new account from the user's email address.",
-          });
-        },
-        onError: (err) => {
-          messageApi.open({
-            type: "error",
-            duration: 5,
-            content: err.message,
-          });
-        },
-      }
-    );
-  };
+	const onFinish = ({ fullName, email, password }) => {
+		if (!isTermsAccepted) {
+			messageApi.open({
+				type: "error",
+				duration: 5,
+				content: "You must accept the terms to continue!",
+			});
+			return;
+		}
 
-  return (
-    <>
-      {contextHolder}
+		signup(
+			{ fullName, email, password },
+			{
+				onSettled: () => form.resetFields(),
+				onSuccess: () => {
+					messageApi.open({
+						type: "success",
+						duration: 5,
+						content:
+							"Account successfully created! Verify new account from the user's email address.",
+					});
+				},
+				onError: (err) => {
+					messageApi.open({
+						type: "error",
+						duration: 5,
+						content: err.message,
+					});
+				},
+			}
+		);
+	};
 
-      <Form form={form} onFinish={onFinish} autoComplete="off">
-        {children}
+	const handleOk = () => {
+		setIsModalOpen(false);
+		setIsTermsAccepted(true);
+	};
 
-        <Form.Item
-          name="fullName"
-          rules={[
-            {
-              required: true,
-              message: "Please input your name!",
-            },
-            {
-              min: 3,
-              message: "Input at least 3 characters!",
-            },
-          ]}
-          style={{ width: "100%", margin: 0 }}
-        >
-          <Input
-            type="text"
-            placeholder="Name"
-            className="auth__input"
-            disabled={isLoading}
-          />
-        </Form.Item>
+	const handleCancel = () => {
+		setIsModalOpen(false);
+		setIsTermsAccepted(false);
+	};
 
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please input valid email!",
-            },
-          ]}
-          style={{ width: "100%", margin: 0 }}
-        >
-          <Input
-            type="email"
-            placeholder="Email"
-            className="auth__input"
-            disabled={isLoading}
-          />
-        </Form.Item>
+	return (
+		<>
+			{contextHolder}
 
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your password!",
-            },
-            {
-              min: 8,
-              message: "Input at least 8 characters!",
-            },
-          ]}
-          style={{ width: "100%", marginBottom: "10px" }}
-        >
-          <Input.Password
-            type="password"
-            placeholder="Password"
-            className="auth__input"
-            disabled={isLoading}
-          />
-        </Form.Item>
+			<Form form={form} onFinish={onFinish} autoComplete="off">
+				{children}
 
-        <Form.Item
-          style={{
-            width: "100%",
-            display: "grid",
-            placeItems: "center",
-            marginBottom: 0,
-          }}
-        >
-          <Button
-            ghost={isLoading}
-            type="primary"
-            shape="round"
-            size="large"
-            className="auth__button"
-            htmlType="submit"
-            disabled={isLoading}
-          >
-            {isLoading ? <Spin /> : "Sign Up"}
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
-  );
+				<Form.Item
+					name="fullName"
+					rules={[
+						{
+							required: true,
+							message: "Please input your name!",
+						},
+						{
+							min: 3,
+							message: "Input at least 3 characters!",
+						},
+					]}
+					style={{ width: "100%", margin: 0 }}
+				>
+					<Input
+						type="text"
+						placeholder="Name"
+						className="auth__input"
+						disabled={isLoading}
+					/>
+				</Form.Item>
+
+				<Form.Item
+					name="email"
+					rules={[
+						{
+							required: true,
+							type: "email",
+							message: "Please input valid email!",
+						},
+					]}
+					style={{ width: "100%", margin: 0 }}
+				>
+					<Input
+						type="email"
+						placeholder="Email"
+						className="auth__input"
+						disabled={isLoading}
+					/>
+				</Form.Item>
+
+				<Form.Item
+					name="password"
+					rules={[
+						{
+							required: true,
+							message: "Please input your password!",
+						},
+						{
+							min: 8,
+							message: "Input at least 8 characters!",
+						},
+					]}
+					style={{ width: "100%", marginBottom: "10px" }}
+				>
+					<Input.Password
+						type="password"
+						placeholder="Password"
+						className="auth__input"
+						disabled={isLoading}
+					/>
+				</Form.Item>
+
+				<Form.Item
+					valuePropName="checked"
+					style={{
+						width: "100%",
+						display: "grid",
+						placeItems: "center",
+						marginBottom: "5px",
+					}}
+				>
+					<Checkbox
+						disabled={isLoading}
+						checked={isTermsAccepted}
+						onChange={() => setIsTermsAccepted((state) => !state)}
+					/>
+					<span className="auth__checkbox">
+						I accept all terms of the{" "}
+						<span className="auth__link" onClick={() => setIsModalOpen(true)}>
+							user agreement
+						</span>
+					</span>
+				</Form.Item>
+
+				<Form.Item
+					style={{
+						width: "100%",
+						display: "grid",
+						placeItems: "center",
+						marginBottom: 0,
+					}}
+				>
+					<Button
+						ghost={isLoading}
+						type="primary"
+						shape="round"
+						size="large"
+						className="auth__button"
+						htmlType="submit"
+						disabled={isLoading}
+					>
+						{isLoading ? <Spin /> : "Sign Up"}
+					</Button>
+				</Form.Item>
+			</Form>
+
+			<Modal
+				title="User agreement"
+				open={isModalOpen}
+				onOk={handleOk}
+				onCancel={handleCancel}
+				style={{ textAlign: "center" }}
+			>
+				<ol className="auth__policy-list">
+					<li>
+						<h2>ПРЕДМЕТ СОГЛАШЕНИЯ</h2>
+						<p>
+							1.1 Настоящее Соглашение предоставляет Клиенту доступ к
+							веб-сервису (далее - Сервис) и регулирует условия его
+							использования.
+						</p>
+					</li>
+					<li>
+						<h2>КОНФИДЕНЦИАЛЬНОСТЬ ИНФОРМАЦИИ</h2>
+						<p>
+							2.1 Клиент признает, что любая информация, предоставленная им при
+							использовании Сервиса, является конфиденциальной и остается его
+							собственностью.
+						</p>
+						<p>
+							2.2 Поставщик обеспечивает конфиденциальность информации Клиента и
+							не разглашает ее третьим лицам без предварительного письменного
+							согласия Клиента, за исключением случаев, предусмотренных
+							законодательством.
+						</p>
+					</li>
+					<li>
+						<h2>ИСПОЛЬЗОВАНИЕ ИНФОРМАЦИИ</h2>
+						<p>
+							3.1 Информация, предоставленная Клиентом, используется Поставщиком
+							исключительно в целях предоставления доступа к Сервису и улучшения
+							его качества.
+						</p>
+						<p>
+							3.2 Поставщик не использует информацию Клиента в целях, не
+							связанных с оказанием услуг, предусмотренных настоящим
+							Соглашением, без предварительного согласия Клиента.
+						</p>
+					</li>
+					<li>
+						<h2>ПРИНЯТИЕ СОГЛАШЕНИЯ</h2>
+						<p>
+							4.1 Нажатие кнопки Принять или использование Сервиса подтверждает
+							ваше ознакомление с настоящим соглашением, понимание его условий и
+							согласие с ними.
+						</p>
+					</li>
+				</ol>
+				<p className="auth__thanks">Благодарим за доверие и использование нашего Сервиса!</p>
+			</Modal>
+		</>
+	);
 }
 
 export default SignupForm;
