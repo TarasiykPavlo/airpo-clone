dd1c8692a84385179f9b1aba6bb95e778cb11bb5
KripakDebug <kastrula123@gmail.com>
2024-03-18 08:07:19 +0200

    add popover

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { Avatar, ConfigProvider, theme } from "antd";
+import { Avatar, ConfigProvider, Popover, theme } from "antd";
 import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
 
 import { useUser } from "../../features/authentication/useUser";
@@ -14,74 +14,79 @@ import "./Profile.scss";
 import { useEffect } from "react";
 
 function Profile() {
-  const { user } = useUser();
-  const {
-    aicoin,
-    avatar,
-    avatar_url,
-    full_name: fullName,
-  } = user.user_metadata;
-  const userId = user.id
-  const { updateUserAicoin } = useUpdateUserAicoin();
-  const { updateUserAvatar, isUpdating } = useUpdateUserAvatar();
+	const { user } = useUser();
+	const {
+		aicoin,
+		avatar,
+		avatar_url,
+		full_name: fullName,
+	} = user.user_metadata;
+	const userId = user.id;
+	const { updateUserAicoin } = useUpdateUserAicoin();
+	const { updateUserAvatar, isUpdating } = useUpdateUserAvatar();
 
-  useEffect(() => {
-    if (typeof aicoin != Number) {
-      updateUserAicoin(aicoin);
-    }
-  }, [aicoin]);
+	useEffect(() => {
+		if (typeof aicoin != Number) {
+			updateUserAicoin(aicoin);
+		}
+	}, [aicoin]);
 
-  return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-      }}
-    >
-      <div className="wrapper">
-        <div className="profile">
-          <div className="profile__head">
-            <div className="user-info">
-              <div className="user-photo">
-                {avatar ? (
-                  <Avatar src={avatar} />
-                ) : avatar_url ? (
-                  <Avatar src={avatar_url} />
-                ) : (
-                  <Avatar icon={<UserOutlined />} />
-                )}
-                <div className="send-photo">
-                  <input
-                    type="file"
-                    name="myImage"
-                    id="sendPhoto"
-                    accept="image/*"
-                    disabled={isUpdating}
-                    onChange={(e) => {
-                      var props = e.target.files[0];
-                      
-                      updateUserAvatar({props, userId});
-                    }}
-                  />
-                  <label htmlFor="sendPhoto">
-                    <img src={iconPencil} />
-                  </label>
-                </div>
-              </div>
-              <div className="user-info__name">{fullName}</div>
-              <div className="user-info__balance">
-                Вalance: {aicoin} AiCoin
-                <ButtonForIcon
-                  icon={<InfoCircleOutlined style={{ color: "#24A1E0" }} />}
-                />
-              </div>
-            </div>
-          </div>
+	return (
+		<ConfigProvider
+			theme={{
+				algorithm: theme.darkAlgorithm,
+			}}
+		>
+			<div className="wrapper">
+				<div className="profile">
+					<div className="profile__head">
+						<div className="user-info">
+							<div className="user-photo">
+								{avatar ? (
+									<Avatar src={avatar} />
+								) : avatar_url ? (
+									<Avatar src={avatar_url} />
+								) : (
+									<Avatar icon={<UserOutlined />} />
+								)}
+								<div className="send-photo">
+									<input
+										type="file"
+										name="myImage"
+										id="sendPhoto"
+										accept="image/*"
+										disabled={isUpdating}
+										onChange={(e) => {
+											var props = e.target.files[0];
 
-          <ProfileCard />
-        </div>
-      </div>
-    </ConfigProvider>
-  );
+											updateUserAvatar({ props, userId });
+										}}
+									/>
+									<label htmlFor="sendPhoto">
+										<img src={iconPencil} />
+									</label>
+								</div>
+							</div>
+							<div className="user-info__name">{fullName}</div>
+							<div className="user-info__balance">
+								Вalance: {aicoin}
+								<Popover
+									content="AiCoin is the internal currency of the site, for which you can purchase goods on the site or receive cashback when withdrawing AiCoin."
+									trigger="click"
+								>
+									<ButtonForIcon
+										icon={<InfoCircleOutlined style={{ color: "#24A1E0" }} />}
+									/>
+								</Popover>
+							</div>
+						</div>
+					</div>
+
+					<ProfileCard />
+				</div>
+			</div>
+		</ConfigProvider>
+	);
 }
 
 export default Profile;
