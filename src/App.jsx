1a8fb9986924d9ef694600dd6cb0a9391d575d06
TarasiykPavlo <117352844+TarasiykPavlo@users.noreply.github.com>
2024-08-17 12:22:43 +0300

    policy + agreemens pages

--- src/App.jsx
+++ src/App.jsx
@@ -19,62 +19,123 @@ import Templates from "./pages/Templates/Templates";
 import NewTemplate from "./pages/NewTemplate/NewTemplate";
 import FAQ from "./pages/FAQ/FAQ";
 import Payment from "./pages/Payment/Payment";
+import UserAgreement from "./pages/UserAgreement/UserAgreement";
+import PrivacyPolicy from "./pages/PrivacyPolicy/PrivacyPolicy";
+
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: 0,
-		},
-	},
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
 });
 
 function App() {
-	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={false} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
 
-			<BrowserRouter>
-				<Routes>
-					<Route
-						element={
-							<ProtectedRoute>
-								<AppLayout />
-							</ProtectedRoute>
-						}
-					>
-						<Route index element={<Navigate replace to="profile" />} />
-						<Route path="profile" element={<Profile />} />
-						<Route path="products" element={<Products />} />
-						<Route path="payment" element={<Payment />} />
-						<Route path="applications" element={<Applications />} />
-						<Route path="faq" element={<FAQ />} />
-						<Route path="applications/new" element={<NewApplication />} />
-						<Route
-							path="applications/new/phone-validation"
-							element={<PhoneValidation />}
-						/>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route
+              index
+              element={
+                <Navigate
+                  replace
+                  to="profile"
+                />
+              }
+            />
+            <Route
+              path="profile"
+              element={<Profile />}
+            />
+            <Route
+              path="products"
+              element={<Products />}
+            />
+            <Route
+              path="payment"
+              element={<Payment />}
+            />
+            <Route
+              path="applications"
+              element={<Applications />}
+            />
+            <Route
+              path="faq"
+              element={<FAQ />}
+            />
+            <Route
+              path="agreement"
+              element={<UserAgreement />}
+            />
+            <Route
+              path="policy"
+              element={<PrivacyPolicy />}
+            />
+            <Route
+              path="applications/new"
+              element={<NewApplication />}
+            />
+            <Route
+              path="applications/new/phone-validation"
+              element={<PhoneValidation />}
+            />
 
-						<Route path="applications/settings" element={<Settings />} />
-						<Route path="applications/bot-settings" element={<BotSettings />} />
-						<Route
-							path="applications/groups-settings"
-							element={<GroupsSettings />}
-						/>
-						<Route path="applications/templates" element={<Templates />} />
-						<Route
-							path="applications/templates/info"
-							element={<NewTemplate />}
-						/>
-					</Route>
+            <Route
+              path="applications/settings"
+              element={<Settings />}
+            />
+            <Route
+              path="applications/bot-settings"
+              element={<BotSettings />}
+            />
+            <Route
+              path="applications/groups-settings"
+              element={<GroupsSettings />}
+            />
+            <Route
+              path="applications/templates"
+              element={<Templates />}
+            />
+            <Route
+              path="applications/templates/info"
+              element={<NewTemplate />}
+            />
+          </Route>
 
-					<Route path="login" element={<Auth />} />
-					<Route path="login/:refid" element={<Auth />} />
-					<Route path="signup" element={<Auth />} />
-					<Route path="signup/:refid" element={<Auth />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-		</QueryClientProvider>
-	);
+          <Route
+            path="login"
+            element={<Auth />}
+          />
+          <Route
+            path="login/:refid"
+            element={<Auth />}
+          />
+          <Route
+            path="signup"
+            element={<Auth />}
+          />
+          <Route
+            path="signup/:refid"
+            element={<Auth />}
+          />
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/ui/Navigation/Navigation.jsx
+++ src/ui/Navigation/Navigation.jsx
@@ -10,111 +10,151 @@ import logo from "./../../assets/logo-icon.svg";
 import "./Navigation.scss";
 
 function Navigation() {
-	const { user } = useUser();
-	const { logout } = useLogout();
-	const { avatar, avatar_url, name } = user.user_metadata;
+  const { user } = useUser();
+  const { logout } = useLogout();
+  const { avatar, avatar_url, name } = user.user_metadata;
 
-	const [isMenuActive, setIsMenuActive] = useState(false);
-	const [matches, setMatches] = useState(
-		window.matchMedia("(max-width: 640px)").matches
-	);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  const [matches, setMatches] = useState(
+    window.matchMedia("(max-width: 640px)").matches
+  );
 
-	useEffect(() => {
-		window
-			.matchMedia("(max-width: 640px)")
-			.addEventListener("change", (e) => setMatches(e.matches));
-	}, []);
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 640px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
 
-	document.body.style.overflow = isMenuActive ? "hidden" : "auto";
+  document.body.style.overflow = isMenuActive ? "hidden" : "auto";
 
-	const menu = (
-		<Menu>
-			<Menu.Item key="0" className="close-menu">
-				<LeftOutlined /> Back
-			</Menu.Item>
-			<div className="user-info">
-				<div className="user-photo">
-					{avatar ? (
-						<Avatar className="menu-avatar" src={avatar} />
-					) : avatar_url ? (
-						<Avatar className="menu-avatar" src={avatar_url} />
-					) : (
-						<Avatar className="menu-avatar" icon={<UserOutlined />} />
-					)}
-				</div>
-				<div className="user-info-main">
-					<div className="user-info__name">{name}</div>
-					<div className="user-info__balance">Вalance: {"0"} AiCoin</div>
-				</div>
-			</div>
-			<Menu.Item key="1">
-				<Link to="https://ai-pro.company/">AiPro</Link>
-			</Menu.Item>
-			<Menu.Item key="2">
-				<Link to={"/profile"}>Profile</Link>
-			</Menu.Item>
-			<Menu.Item key="4">
-				<Link to={"/products"}>Shop</Link>
-			</Menu.Item>
-			<Menu.Item key="5">
-				<Link to={"/applications"}>My products</Link>
-			</Menu.Item>
-			<Menu.Item key="3">
-				<Link to={"/faq"}>FAQ</Link>
-			</Menu.Item>
-			<Menu.Item onClick={logout} key="6">
-				Logout
-			</Menu.Item>
-		</Menu>
-	);
+  const menu = (
+    <Menu>
+      <Menu.Item
+        key="0"
+        className="close-menu"
+      >
+        <LeftOutlined /> Back
+      </Menu.Item>
+      <div className="user-info">
+        <div className="user-photo">
+          {avatar ? (
+            <Avatar
+              className="menu-avatar"
+              src={avatar}
+            />
+          ) : avatar_url ? (
+            <Avatar
+              className="menu-avatar"
+              src={avatar_url}
+            />
+          ) : (
+            <Avatar
+              className="menu-avatar"
+              icon={<UserOutlined />}
+            />
+          )}
+        </div>
+        <div className="user-info-main">
+          <div className="user-info__name">{name}</div>
+          <div className="user-info__balance">Вalance: {"0"} AiCoin</div>
+        </div>
+      </div>
+      <Menu.Item key="1">
+        <Link to="https://ai-pro.company/">AiPro</Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Link to={"/profile"}>Profile</Link>
+      </Menu.Item>
+      <Menu.Item key="4">
+        <Link to={"/products"}>Shop</Link>
+      </Menu.Item>
+      <Menu.Item key="5">
+        <Link to={"/applications"}>My products</Link>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Link to={"/faq"}>FAQ</Link>
+      </Menu.Item>
+      <Menu.Item key="7">
+        <Link to={"/agreement"}>User Agreement</Link>
+      </Menu.Item>
+      <Menu.Item key="8">
+        <Link to={"/policy"}>Privacy Policy</Link>
+      </Menu.Item>
+      <Menu.Item
+        onClick={logout}
+        key="6"
+      >
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
 
-	return (
-		<ConfigProvider
-			theme={{
-				algorithm: theme.darkAlgorithm,
-			}}
-		>
-			<nav className="nav">
-				<Link to="/profile" className="logo">
-					AiPro <img src={logo} alt="Logo AiPro" />
-				</Link>
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <nav className="nav">
+        <Link
+          to="/profile"
+          className="logo"
+        >
+          AiPro{" "}
+          <img
+            src={logo}
+            alt="Logo AiPro"
+          />
+        </Link>
 
-				<div className="user" style={{ cursor: "pointer" }}>
-					<Select
-						defaultValue="En"
-						className="change-lang"
-						options={[
-							{
-								value: "En",
-								label: "En",
-							},
-						]}
-					/>
-					<Dropdown
-						overlayClassName="dropdown-menu"
-						overlay={menu}
-						onOpenChange={(e) => matches && setIsMenuActive(e)}
-						placement={matches ? "bottom" : "bottomRight"}
-						trigger={["click"]}
-					>
-						{matches ? (
-							<div className="burger">
-								<div className="burger-item"></div>
-								<div className="burger-item"></div>
-								<div className="burger-item"></div>
-							</div>
-						) : avatar ? (
-							<Avatar className="avatar" src={avatar} />
-						) : avatar_url ? (
-							<Avatar className="avatar" src={avatar_url} />
-						) : (
-							<Avatar className="avatar" icon={<UserOutlined />} />
-						)}
-					</Dropdown>
-				</div>
-			</nav>
-		</ConfigProvider>
-	);
+        <div
+          className="user"
+          style={{ cursor: "pointer" }}
+        >
+          <Select
+            defaultValue="En"
+            className="change-lang"
+            options={[
+              {
+                value: "En",
+                label: "En",
+              },
+            ]}
+          />
+          <Dropdown
+            overlayClassName="dropdown-menu"
+            overlay={menu}
+            onOpenChange={(e) => matches && setIsMenuActive(e)}
+            placement={matches ? "bottom" : "bottomRight"}
+            trigger={["click"]}
+          >
+            {matches ? (
+              <div className="burger">
+                <div className="burger-item"></div>
+                <div className="burger-item"></div>
+                <div className="burger-item"></div>
+              </div>
+            ) : avatar ? (
+              <Avatar
+                className="avatar"
+                src={avatar}
+              />
+            ) : avatar_url ? (
+              <Avatar
+                className="avatar"
+                src={avatar_url}
+              />
+            ) : (
+              <Avatar
+                className="avatar"
+                icon={<UserOutlined />}
+              />
+            )}
+          </Dropdown>
+        </div>
+      </nav>
+    </ConfigProvider>
+  );
 }
 
 export default Navigation;
